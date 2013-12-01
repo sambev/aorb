7da351bbc950a35a1efacfb2e969ca68deeb0d7e
Sam Beveridge <sam.bev87@gmail.com>
2013-12-01 15:38:39 -0700

    made the angular app use directivess

--- dashboard/angular/dashboard.js
+++ dashboard/angular/dashboard.js
@@ -17,7 +17,7 @@ app.factory('Widgets', function() {
     return Widgets;
 })
 
-function widgetCtrl($scope, Widgets) {
+app.controller('widgetCtrl', function ($scope, Widgets) {
     $scope.widgets = Widgets;
     $scope.addOne = function(index) {
         Widgets.data[index].content++;
@@ -25,4 +25,24 @@ function widgetCtrl($scope, Widgets) {
     $scope.rmOne = function(index) {
         Widgets.data[index].content--;
     };
-}
+})
+
+app.directive('widget', function() {
+    return {
+        restrict: 'E',
+        template: '<h3>{{ widget.title }}</h3>\
+            <p>{{ widget.type }}</p>\
+            <p>{{ widget.content }}</p>\
+            <button ng-click="addOne($index)">Add</button>\
+            <button ng-click="rmOne($index)">Remove</button>'
+    }
+})
+
+app.directive('widgetchart', function() {
+    return {
+        restrict: 'E',
+        template: '<div class="bar" style="width:{{ widget.content }}0px">\
+            {{ widget.title }} : {{ widget.content }}\
+            </div>'
+    }
+})
